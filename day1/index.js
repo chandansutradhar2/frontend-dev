ad2f859d220e17b72af3391aff83d72732d33652
chandan_naresh <chandansutradhar2@gmail.com>
2022-01-28 14:46:13 +0530

    async vs sync page load

--- day1/index.js
+++ day1/index.js
@@ -1,4 +1,20 @@
-var name = "chandan";
-console.log(name);
+function login() {
+	//lesson 3: async function call here... any function which makes the process wait
+	// setTimeout(() => {
+	// 	console.log("login invoked");
+	// }, 10000);
+	//sync code
+	let n = 0;
+	console.log("before foreach loop :", n);
+	//for each ..loop are not considered async function
+	for (let i = 0; i < 10000000; i++) {
+		n++;
+	}
+	console.log("after for each completion value of n is :", n);
+}
 
-console.log("some name");
+//function call / function invocation
+login();
+
+//lesson 1: JS is functional programming
+//lesson 2: JS is OOPS as well ...to be proven
