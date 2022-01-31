0bf8c38816f69e20a439770162c70b57a75f95f3
chandan_naresh <chandansutradhar2@gmail.com>
2022-01-31 10:14:29 +0530

    async demo

--- day3/async-await.js
+++ day3/async-await.js
@@ -1,42 +1,60 @@
-function fun1() {
+async function fun1() {
 	return new Promise((resolve, reject) => {
-		console.log("fun 1 invoked");
-		resolve("fun1");
+		setTimeout(() => {
+			console.log("fun 1 invoked");
+			resolve(10);
+		}, 2000);
 	});
 }
 
-function fun2() {
+async function fun2() {
 	return new Promise((resolve, reject) => {
-		console.log("fun 2 invoked");
-		resolve("fun2");
+		setTimeout(() => {
+			console.log("fun 2 invoked");
+			resolve(20);
+		}, 4000);
 	});
 }
 
-function fun3() {
+async function fun3() {
 	return new Promise((resolve, reject) => {
-		console.log("fun 3 invoked");
-		resolve("fun3");
+		setTimeout(() => {
+			console.log("fun 3 invoked");
+			resolve("fun3");
+		}, 1200);
 	});
 }
 
-function fun4() {
+async function fun4() {
 	return new Promise((resolve, reject) => {
-		console.log("fun 4 invoked");
-		resolve("fun4");
+		setTimeout(() => {
+			console.log("fun 4 invoked");
+			resolve("fun4");
+		}, 4800);
 	});
 }
 
-function fun5() {
+async function fun5() {
 	return new Promise((resolve, reject) => {
-		console.log("fun 5 invoked");
-		resolve("fun5");
+		setTimeout(() => {
+			console.log("fun 5 invoked");
+			resolve("fun5");
+		}, 2300);
 	});
 }
 
-function callerFun() {
-	fun1().then(() =>
-		fun2().then(() => fun3().then(() => fun4().then(() => fun5().then()))),
-	);
+async function callerFun() {
+	// fun1().then(() =>
+	// 	fun2().then(() => fun3().then(() => fun4().then(() => fun5().then()))),
+	// );
+
+	const res1 = await fun1();
+	const res2 = await fun2();
+	const res3 = await fun3();
+	const res4 = await fun4();
+	const res5 = await fun5();
+
+	console.log(res1, res2, res3, res4, res5);
 }
 
 callerFun();
