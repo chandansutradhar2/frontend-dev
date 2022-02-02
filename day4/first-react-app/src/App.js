8e0a1e8a6222f969ae5d9c818963e4e6ed7329de
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-02 10:01:43 +0530

    importing component

--- day4/first-react-app/src/App.js
+++ day4/first-react-app/src/App.js
@@ -1,21 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Title from "./Header";
+import * as demo from "./Header";
+import { FirstName } from "./Header";
+import { SideBar } from "./Sidebar";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p> have edited new line here.</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="container">
+			<Title />
+			<SideBar />
 		</div>
 	);
 }
