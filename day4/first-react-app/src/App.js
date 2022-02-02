0f0db49a5ac4dfe4be7f300f02f738ee071b5c25
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-02 14:57:37 +0530

    expression within JSX demo

--- day4/first-react-app/src/App.js
+++ day4/first-react-app/src/App.js
@@ -1,15 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./Header";
-import { SideBar } from "./Sidebar";
+//import { SideBar } from "./Sidebar";
 import { Login } from "./Login";
 
 function App() {
+	var cnt = 0;
+	let products = [
+		{ name: "samasung galaxy X", price: 30000, qty: 0 },
+		{ name: "samasung galaxy Y", price: 56000, qty: 25 },
+		{ name: "samasung galaxy Z", price: 75000, qty: 22 },
+	];
 	return (
-		<div className="container">
-			{/* <Title />
-			<SideBar /> */}
-			<Login />
+		<div>
+			<ul>
+				{products.map((ele) => {
+					return <li>{ele.name}</li>;
+				})}
+			</ul>
 		</div>
 	);
 }
--- day4/first-react-app/src/Login.js
+++ day4/first-react-app/src/Login.js
@@ -1,8 +1,39 @@
+import { Button, Form } from "react-bootstrap";
 export function Login() {
+	function doLogin() {
+		alert("login clicked");
+		console.log(email);
+	}
+
+	//declare variables
+	var email = "chandan@gmail.com";
+
 	return (
 		<>
 			<h2>Login </h2>
-			<div className="container">Login UI goes here</div>
+			{/* usage of email in jsx */}
+			<h4>{email}</h4>
+			<div>
+				The Current time is
+				{new Date().toISOString()}
+			</div>
+			<Form>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control type="email" placeholder="Enter email" />
+				</Form.Group>
+
+				<Form.Group className="mb-3" controlId="formBasicPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control type="password" placeholder="Password" />
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="formBasicCheckbox">
+					<Form.Check type="checkbox" label="Remember Met" />
+				</Form.Group>
+				<Button variant="primary" onClick={() => doLogin()}>
+					Login
+				</Button>
+			</Form>
 		</>
 	);
 }
