8d4b3919cf4107223367f38639a4072e8f344079
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-03 15:11:13 +0530

    useState setup and usage demo

--- day6/expense-tracker/src/components/Expenses/ExpenseItem.js
+++ day6/expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,20 @@
 import "./ExpenseItem.css";
+import React, { useState } from "react";
+
 export const ExpenseItem = (props) => {
-	console.log(props);
-	let date = new Date(props.expense.expDate);
+	const [amount, setAmount] = useState(props.expense.amount);
+	const [title, setTile] = useState(props.expense.title);
+	const [expDate, setExpDate] = useState(props.expense.expDate);
+
+	//DO NOT CHANGE PROPS DIRECTLY EVEN IF ITS AN OBJECT
+	// console.log(props.expense.amount);
+
+	// setTimeout(() => {
+	// 	setAmount(amount + 500);
+	// 	console.log("new value", props.expense.amount);
+	// }, 5000);
+
+	let date = new Date(expDate);
 	let month = date.toLocaleDateString("default", { month: "short" });
 	let day = date.toLocaleDateString("default", { day: "2-digit" });
 	return (
@@ -12,8 +25,8 @@ export const ExpenseItem = (props) => {
 					<span>{day}</span>&nbsp;
 					<strong>{month}</strong>
 				</div>
-				<div>{props.expense.title}</div>
-				<div>&#8377; {props.expense.amount}</div>
+				<div>{title}</div>
+				<div>&#8377; {amount}</div>
 			</div>
 		</div>
 	);
