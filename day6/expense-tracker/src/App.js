45c7745514beaf34a9fbd752e518c9d4d4a1bf71
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-03 12:00:11 +0530

    added datatable

--- day6/expense-tracker/src/App.js
+++ day6/expense-tracker/src/App.js
@@ -8,49 +8,34 @@ import {
 	INVESTMENT_FREQUENCY,
 	INVESTMENT_TYPE,
 } from "./models/Investment.model";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 function App() {
-	function addInvestment() {
-		let vst = new Investment();
-		vst.investmentCategory = INVESTMENT_CATEGORY.MF;
-		vst.amount = 5000;
-		vst.frequency = INVESTMENT_FREQUENCY.MONTHLY;
-		vst.investmentType = INVESTMENT_TYPE.SIP;
-		vst.createdBy = "";
-		vst.createdOn = new Date();
-		vst.id = "002";
-		vst.startDate = new Date("2020");
-		vst.endDate = new Date("2030");
-		console.log(vst);
-	}
-	addInvestment();
+	let products = [
+		{
+			code: "c001",
+			name: "Demo product",
+			category: "Some Category",
+			quantity: 10,
+		},
+	];
 	return (
 		<div className="container">
-			<h5>Toggleable</h5>
-			<Fieldset legend="Header" toggleable>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum.
-				</p>
-				<Button label="Add Expense" className="p-button-danger" />
-			</Fieldset>
 			<div className="card">
-				<div className="card-container blue-container flex align-items-center justify-content-start">
-					<div className="hidden w-4rem h-4rem bg-blue-500 text-white font-bold p-4 border-round mr-3">
-						1
-					</div>
-					<div className="w-4rem h-4rem bg-blue-500 text-white font-bold flex align-items-center justify-content-center p-4 border-round mr-3">
-						2
-					</div>
-					<div className="w-4rem h-4rem bg-blue-500 text-white font-bold flex align-items-center justify-content-center p-4 border-round">
-						3
-					</div>
-				</div>
+				<Fieldset legend="Add Income/Expenses" toggleable>
+					<p></p>
+					<Button label="Add Expense" className="p-button-danger" />
+				</Fieldset>
+			</div>
+			<hr />
+			<div className="card">
+				<DataTable value={products}>
+					<Column field="code" header="Code"></Column>
+					<Column field="name" header="Name"></Column>
+					<Column field="category" header="Category"></Column>
+					<Column field="quantity" header="Quantity"></Column>
+				</DataTable>
 			</div>
 		</div>
 	);
