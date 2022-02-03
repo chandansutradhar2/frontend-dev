5abc4ba5c42a67b926a9f1a277f426e2054779d1
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-03 14:52:37 +0530

    css formatting for exp item

--- day6/expense-tracker/src/components/Expenses/ExpenseItem.js
+++ day6/expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,20 @@
-export const ExpenseItem = () => {
-	return <div>Expense Item</div>;
+import "./ExpenseItem.css";
+export const ExpenseItem = (props) => {
+	console.log(props);
+	let date = new Date(props.expense.expDate);
+	let month = date.toLocaleDateString("default", { month: "short" });
+	let day = date.toLocaleDateString("default", { day: "2-digit" });
+	return (
+		<div style={{ margin: "0.5rem", padding: "0.5rem" }}>
+			<div className="expense">
+				<div>
+					<i className="pi pi-calendar"></i>
+					<span>{day}</span>&nbsp;
+					<strong>{month}</strong>
+				</div>
+				<div>{props.expense.title}</div>
+				<div>&#8377; {props.expense.amount}</div>
+			</div>
+		</div>
+	);
 };
