ee5320ddc47901d4e1214f907c6695f07cfd89c9
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-03 11:52:43 +0530

    enum as class demo

--- day6/expense-tracker/src/models/Expense.model.js
+++ day6/expense-tracker/src/models/Expense.model.js
@@ -3,4 +3,14 @@ export class Expense {
 	createdBy;
 	category;
 	amount;
+	transactionType;
+}
+
+export class TRANSACTION_TYPE {
+	static CREDIT = new TRANSACTION_TYPE("CREDIT");
+	static DEBIT = new TRANSACTION_TYPE("DEBIT");
+
+	constructor(name) {
+		this.name = name;
+	}
 }
