1cdb6ca8723234510f92ec77d28422a88a447152
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-11 13:15:37 +0530

    sending token from server

---
+++ project works/finance-guru-node-server/controller/user.controller.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require("mongodb");
+const jwt = require("jsonwebtoken");
+
+const client = new MongoClient(
+	"mongodb+srv://admin:hisyCn$AhkX5Ggz@lab-cluster-1.ihxrn.mongodb.net/test?authSource=admin&replicaSet=atlas-nmhvza-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+);
+
+exports.getUserByEmail = (req, res) => {
+	if (req.body.email && req.body.password) {
+		client.connect(async () => {
+			//initialize connection with database collection
+			const cursor = await client.db("finance-guru").collection("users").find({
+				email: req.body.email,
+				password: req.body.password,
+			});
+			//wait for read to complete
+			const docs = await cursor.toArray();
+			//check to ensure if the documents array has any document in it
+			if (docs.length > 0) {
+				//return token
+				let token = jwt.sign({ user: docs }, "somesecret", {
+					expiresIn: "1h",
+				});
+				res.status(200).send({ result: true, token: token });
+			} else {
+				res.status(401).send("invalid credentials");
+			}
+		});
+	} else {
+		res.status(401).send("email and password is required");
+	}
+};
--- project works/react-finance-guru/src/Auth/Login/Login.js
+++ project works/react-finance-guru/src/Auth/Login/Login.js
@@ -1,3 +1,58 @@
+import { useState } from "react";
+
 export const Login = () => {
-	return <h1>Login Component</h1>;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleLogin = () => {
+		//TO DO : LOGIN behaviour
+	};
+
+	return (
+		<div style={{ width: "100vw", display: "flex", justifyContent: "center" }}>
+			<form style={{ width: "50%", paddingTop: "10%" }}>
+				<div className="mb-3">
+					<label className="form-label">Email address</label>
+					<input
+						type="email"
+						className="form-control"
+						id="exampleFormControlInput1"
+						placeholder="name@example.com"
+						value={email}
+						onChange={(ev) => setEmail(ev.target.value)}
+					/>
+					{email.length <= 0 ? (
+						<div style={{ color: "red", fontSize: "small" }}>
+							Email Cannot be left blank
+						</div>
+					) : null}
+				</div>
+
+				<div className="mb-3">
+					<label className="form-label">Password</label>
+					<input
+						type="password"
+						className="form-control"
+						id="exampleFormControlInput1"
+						value={password}
+						onChange={(ev) => setPassword(ev.target.value)}
+					/>
+					{password.length <= 0 ? (
+						<div style={{ color: "red", fontSize: "small" }}>
+							Password Cannot be left blank
+						</div>
+					) : null}
+				</div>
+				<div className="mb-3">
+					<button
+						type="button"
+						className="btn btn-primary"
+						onClick={handleLogin}
+					>
+						Login
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 };
